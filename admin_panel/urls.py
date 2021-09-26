cd13ac17e1864daf500b9e2097cacb6b9a0a620d
rabihofcl <rabihofcl@gmail.com>
2021-09-27 00:07:25 +0530

    products edits completed

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
     path('', views.admin_signin, name='admin_signin'),
     path('admin_home', views.admin_home, name='admin_home'),
     path('ad_brand_list', views.ad_brand_list, name='ad_brand_list'),
+    path('ad_delete_brand/', views.ad_delete_brand, name='ad_delete_brand'),
+    # path('ad_brand_edit/<id>', views.ad_brand_edit, name='ad_brand_edit'),
     path('ad_add_brand', views.ad_add_brand, name='ad_add_brand'),
     path('ad_product_list', views.ad_product_list, name='ad_product_list'),
-    path('ad_delete_product/', views.ad_delete_product, name='ad_delete_product'),
+    path('ad_product_edit/<id>', views.ad_product_edit, name='ad_product_edit'),
+    path('ad_delete_product/', views.ad_delete_product, name='ad_delete_product'), 
     path('ad_add_product', views.ad_add_product, name='ad_add_product'),
     path('active_users', views.active_users, name='active_users'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -6,6 +6,7 @@ from account.models import Account
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import auth
+import os
 
 # Create your views here.
 
@@ -41,7 +42,6 @@ def ad_brand_list(request):
     return render(request, 'ad_brand_list.html', {'brands': brands})
 
 
-
 def ad_add_brand(request):
     if request.method == 'POST':
         form = BrandForm(request.POST, request.FILES)
@@ -58,14 +58,54 @@ def ad_add_brand(request):
         return render(request, 'ad_add_brand.html', {'form': form})
 
 
+def ad_delete_brand(request):
+    id = request.POST['id']
+    Brand.objects.filter(id=id).delete()
+    return JsonResponse({'success': True})
+
+# def ad_brand_edit(request, slug):
+#     if request.method == 'POST':
+#         brand = Brand.objects.get(slug=slug)
+#         form = BrandForm(request.POST, request.FILES, instance='brand')
+
+#         if form.is_valid():
+#             form.save()
+#             return redirect ('ad_brand_list')
+
+#         else:
+#             form = BrandForm(instance=brand)
+#             return render (request, 'ad-brand-edit', {'brand':brand, 'form':form})
+
+
+#         if Product.objects.exclude(id=id).filter(product_name=product_name).exists():
+#             messages.info(request, 'Product already exist')
+#             return redirect('ad_product_edit', id=id)
+#         else:
+#             Product.objects.filter(pk=id).update(product_name=product_name, brand=brand, description=description, price=price,
+#                                                  stock=stock, image1=image1, image2=image2, image3=image3, image4=image4, is_available=True, slug=slug)
+#             return redirect('ad_product_list')
+
+    # else:
+    #     brands = Brand.objects.all()
+    #     product = Product.objects.get(pk=id)
+
+    #     context = {
+    #         'product': product,
+    #         'brands': brands,
+    #     }
+    #     return render(request, 'ad_product_edit.html', context)
+
+
+
 
 def ad_product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-id')
     return render(request, 'ad_product_list.html', {'products': products})
 
 
 def ad_add_product(request):
     if request.method == 'POST':
+
         product_name = request.POST['product_name']
         brand = Brand.objects.get(brand_name=request.POST['brand'])
         description = request.POST['description']
@@ -75,24 +115,64 @@ def ad_add_product(request):
         image2 = request.FILES['image2']
         image3 = request.FILES['image3']
         image4 = request.FILES['image4']
-        slug = product_name.lower()
+        slug = product_name.lower().replace(" ","-")
 
-        product = Product(product_name=product_name, brand=brand, description=description,
-                          price=price, stock=stock, image1=image1, image2=image2 ,image3=image3,image4=image4, is_available=True, slug=slug)
+        product = Product(product_name=product_name, brand=brand, description=description, price=price,
+                          stock=stock, image1=image1, image2=image2, image3=image3, image4=image4, is_available=True, slug=slug)
         product.save()
         return redirect('ad_add_product')
 
     brands = Brand.objects.all()
     return render(request, 'ad_add_product.html', {'brands': brands})
 
+
+def ad_product_edit(request, id):
+    product = Product.objects.get(pk=id)
+
+    if request.method == 'POST':
+        if len(request.FILES) != 0:
+            if len(product.image1) > 0:
+                os.remove(product.image1.path)
+            product.image1 = request.FILES['image1']
+            if len(product.image2) > 0:
+                os.remove(product.image2.path)
+            product.image2 = request.FILES['image2']
+            if len(product.image3) > 0:
+                os.remove(product.image3.path)
+            product.image3 = request.FILES['image3']
+            if len(product.image4) > 0:
+                os.remove(product.image4.path)
+            product.image4 = request.FILES['image4']
+
+        product_name = request.POST['product_name']
+        brand = Brand.objects.get(brand_name=request.POST['brand'])
+        description = request.POST['description']
+        price = request.POST['price']
+        stock = request.POST['stock']
+        slug = product_name.lower().replace(" ","-")
+
+        if Product.objects.exclude(id=id).filter(product_name=product_name).exists():
+            messages.info(request, 'Product already exist')
+            return redirect('ad_product_edit', id=id)
+        else:
+            product.save()
+            return redirect('ad_product_list')
+
+    else:
+        brands = Brand.objects.all()
+        context = {
+            'product': product,
+            'brands': brands,
+        }
+        return render(request, 'ad_product_edit.html', context)
+
+
 def ad_delete_product(request):
-    id=request.POST['id']
+    id = request.POST['id']
     Product.objects.filter(id=id).delete()
-    return JsonResponse({'success':True})
+    return JsonResponse({'success': True})
 
 
 def active_users(request):
     users = Account.objects.order_by('id').all()
     return render(request, 'active_users.html', {'users': users})
-
-
