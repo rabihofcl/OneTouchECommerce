7c0c9d9855a98e90b5a0bf852534a77c53968871
rabihofcl <rabihofcl@gmail.com>
2021-10-05 13:33:43 +0530

    payment backend added

--- orders/views.py
+++ orders/views.py
@@ -2,12 +2,57 @@ from django.shortcuts import redirect, render
 from cart.models import CartItem
 from .forms import OrderForm
 import datetime
-from .models import Order
+from .models import Order, OrderProduct, Payment
+import json
 
 # Create your views here.
 
 
 def payments(request):
+    body = json.loads(request.body)
+    print(body)
+
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+
+    #store transaction details inside payment model
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    # Move the cart items to Order Product Table
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+
+    # Reduce the quantity of the sold products
+
+
+    # Clear cart
+
+
+    # Send order recieved email to customer
+
+    
+
+
     return render(request, 'payments.html')
 
 
